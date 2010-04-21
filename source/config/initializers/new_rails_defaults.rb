86474e7bd51a25e946010b1e5c36ef89a42fbeb8
Piotrek <piotr.bator@river.com.pl>
2010-04-21 15:48:44 +0200

    Drobna poprawka w konfiguracji to_json

--- source/config/initializers/new_rails_defaults.rb
+++ source/config/initializers/new_rails_defaults.rb
@@ -5,7 +5,7 @@
 
 if defined?(ActiveRecord)
   # Include Active Record class name as root for JSON serialized output.
-  ActiveRecord::Base.include_root_in_json = true
+  ActiveRecord::Base.include_root_in_json = false
 
   # Store the full class name (including module namespace) in STI type column.
   ActiveRecord::Base.store_full_sti_class = true
@@ -19,5 +19,3 @@ ActiveSupport.use_standard_json_time_format = true
 # Don't escape HTML entities in JSON, leave that for the #json_escape helper.
 # if you're including raw json in an HTML page.
 ActiveSupport.escape_html_entities_in_json = false
-
-ActiveRecord::Base.include_root_in_json = false

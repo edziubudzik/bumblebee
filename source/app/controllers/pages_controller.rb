f232604437e81f99b765a87c1fad5774a40d16a0
Piotrek <piotr.bator@river.com.pl>
2010-04-27 14:21:50 +0200

    Wyświetlanie starszych wersji stron

--- source/app/controllers/pages_controller.rb
+++ source/app/controllers/pages_controller.rb
@@ -3,4 +3,16 @@ class PagesController < ApplicationController
   resource_controller
   
   belongs_to :project
+  
+  create.before do
+    params[:page][:user_id] = current_user.id
+  end
+  
+  update.before do
+    params[:page][:user_id] = current_user.id
+  end
+  
+  show.before do
+    @page.revert_to(params[:version].to_i) if params[:version]
+  end
 end

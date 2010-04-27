4dbf6ec2f1e3d0323b3c6d50d0e6c0b2acdb93ef
Piotrek <piotr.bator@river.com.pl>
2010-04-27 14:27:02 +0200

    Poprawne przypisywanie użytkownika do strony.

--- source/app/controllers/pages_controller.rb
+++ source/app/controllers/pages_controller.rb
@@ -4,14 +4,6 @@ class PagesController < ApplicationController
   
   belongs_to :project
   
-  create.before do
-    params[:page][:user_id] = current_user.id
-  end
-  
-  update.before do
-    params[:page][:user_id] = current_user.id
-  end
-  
   show.before do
     @page.revert_to(params[:version].to_i) if params[:version]
   end

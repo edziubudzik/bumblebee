55f3f4c24a5f9aafa39d157dac2f04e7430f5b6f
mgmt <MG@.(none)>
2010-04-29 10:33:41 +0200

    Bardzo bardzo bardzo wstepna wersja blocks

--- source/app/controllers/blocks_controller.rb
+++ source/app/controllers/blocks_controller.rb
@@ -1,4 +1,22 @@
 class BlocksController < ApplicationController
-  resource_controller
-  belongs_to :project
+  #resource_controller
+  #belongs_to :project
+
+  before_filter :find_project
+
+  def new
+    @block = Block.new
+
+  end
+
+  def create
+    @block = Block.new
+    #@block.project_id = params[:project_id]
+
+  end
+
+  private
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
 end

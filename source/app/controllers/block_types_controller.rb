c4fe3f40928aff73db1f841117aeaa90546ed0d5
mgmt <MG@.(none)>
2010-05-02 21:36:29 +0200

    Dokonczenie CRUD dla block_types (w tym dodawanie stage_types do block_types wraz z nakladem pracy)

--- source/app/controllers/block_types_controller.rb
+++ source/app/controllers/block_types_controller.rb
@@ -1,14 +1,51 @@
 class BlockTypesController < ApplicationController
   resource_controller
 
-  before_filter :find_stage_types
+  before_filter :create_empty_arrays
   before_filter :ensure_no_stage_type, :only => :update
 
   create.wants.js { redirect_to block_types_path }
 
+
+
+  show.before do
+    (BlockTypeStageType.find_all_by_block_type_id @block_type.id).each do |btst|
+      @stage_types.push(StageType.find(btst.stage_type_id))
+      @stage_types_costs[btst.stage_type_id] = btst.cost
+    end
+  end
+
+  create.after do
+    (BlockTypeStageType.find_all_by_block_type_id @block_type.id).each do |btst|
+      BlockTypeStageType.update(btst.id, {:cost => params[:stage_types_costs][btst.stage_type_id.to_s]})
+    end
+  end
+
+  edit.before do
+    (BlockTypeStageType.find_all_by_block_type_id @block_type.id).each do |btst|
+      @stage_types_costs[btst.stage_type_id] = btst.cost
+    end
+  end
+  
+  update.after do
+    (BlockTypeStageType.find_all_by_block_type_id @block_type.id).each do |btst|
+      BlockTypeStageType.update(btst.id, {:cost => params[:stage_types_costs][btst.stage_type_id.to_s]})
+    end
+  end
+
+  destroy.after do
+    (BlockTypeStageType.find_all_by_block_type_id @block_type.id).each do |btst|
+      btst.destroy
+    end
+  end
+
+
+
   private
-  def find_stage_types
-    @stage_types = StageType.find :all
+
+  def create_empty_arrays
+    @stage_types = []
+    @stage_types_costs = []
   end
 
   def ensure_no_stage_type

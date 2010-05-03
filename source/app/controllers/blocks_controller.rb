193265b7e3396d3c6514f28dfdd9cca739952c44
mgmt <MG@.(none)>
2010-05-03 21:15:43 +0200

    Poczatki prac przy blocks

--- source/app/controllers/blocks_controller.rb
+++ source/app/controllers/blocks_controller.rb
@@ -1,22 +1,19 @@
 class BlocksController < ApplicationController
-  #resource_controller
-  #belongs_to :project
+  resource_controller
+  belongs_to :project
 
   before_filter :find_project
-
-  def new
-    @block = Block.new
-
-  end
-
-  def create
-    @block = Block.new
-    #@block.project_id = params[:project_id]
-
-  end
+  before_filter :find_block_type
 
   private
+
   def find_project
     @project = Project.find(params[:project_id])
   end
+
+  def find_block_type
+    @block = Block.find(params[:id])
+    @block_type = BlockType.find(@block.block_type_id)
+  end
+
 end
